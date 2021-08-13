37f9cca38bcc2d2241abaca6826f1dd104a6132f
DOMAIN\benson.chung <benson.chung@sportscity.com.tw>
2021-08-13 16:56:32 +0800

    加入Final Inspection的Controller Action

---
+++ Quality/Quality/Areas/FinalInspection/Controllers/QueryController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quality.Areas.FinalInspection.Controllers
+{
+    public class QueryController : Controller
+    {
+        // GET: FinalInspection/Query
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
