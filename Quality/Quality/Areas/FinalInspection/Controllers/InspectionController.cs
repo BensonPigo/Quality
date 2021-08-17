5bac2e9381625109b257fa3268fc14cd44e03748
DOMAIN\benson.chung <benson.chung@sportscity.com.tw>
2021-08-17 17:45:15 +0800

    調整Layout JS檔案位置

--- Quality/Quality/Areas/FinalInspection/Controllers/InspectionController.cs
+++ Quality/Quality/Areas/FinalInspection/Controllers/InspectionController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DatabaseObject.ResultModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,7 @@ namespace Quality.Areas.FinalInspection.Controllers
         // Setting
         public ActionResult Index()
         {
-            return View();
+            return View(new UserList());
         }
 
         public ActionResult Setting()
