bc448ffa9fda60ed2eaf1b6af6fe7be2dd46e26b
DOMAIN\jeff.yeh <Jeff.Yeh@sportscity.com.tw>
2021-08-17 22:07:54 +0800

    ISP20211028 Sample RFT - CFT Comments [後端]

---
+++ Quality/BusinessLogicLayerTests/Service/CFTCommentsTests.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLogicLayer.Interface;
+using DatabaseObject.ViewModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace BusinessLogicLayer.Service.Tests
+{
+    [TestClass()]
+    public class CFTCommentsTests
+    {
+        [TestMethod()]
+        public void GetRFT_OrderComments()
+        {
+            try
+            {
+                ICFTCommentsService _CFTCommentsService = new CFTCommentsService();
+                CFTComments_where CFTComments = new CFTComments_where() { OrderID = "20120739GGS01"};
+                var _Para = _CFTCommentsService.GetRFT_OrderComments(CFTComments);
+                Assert.IsTrue(_Para.Count == 1 );
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.ToString());
+            }
+        }
+    }
+}
---
+++ Quality/ManufacturingExecutionDataAccessLayer/Interface/ICFTCommentsProvider.cs
@@ -0,0 +1,17 @@
+﻿using DatabaseObject.ManufacturingExecutionDB;
+using DatabaseObject.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManufacturingExecutionDataAccessLayer.Interface
+{
+    public interface ICFTCommentsProvider
+    {
+        IList<CFTComments_where> GetCFT_Orders(CFTComments_where CFTComments);
+
+        IList<CFTComments_ViewModel> GetRFT_OrderComments(CFTComments_where CFTComments);
+    }
+}
