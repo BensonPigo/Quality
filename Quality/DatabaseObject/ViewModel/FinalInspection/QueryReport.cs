c9a5bbf3aa4335a48adc309010a814ed378f5ac4
DOMAIN\aaron.shie <aaron.shie@sportscity.com.tw>
2021-08-18 14:01:05 +0800

    ISP20211021 Quality Web-Final Inspection Feature(module: Query) [後端] FinalInspection QueryReport Model更新

---
+++ Quality/DatabaseObject/ViewModel/FinalInspection/QueryReport.cs
@@ -0,0 +1,29 @@
+﻿using DatabaseObject.ManufacturingExecutionDB;
+using DatabaseObject.ProductionDB;
+using System;
+using System.Collections.Generic;
+
+namespace DatabaseObject.ViewModel.FinalInspection
+{
+    public class QueryReport
+    {
+        public ManufacturingExecutionDB.FinalInspection FinalInspection { get; set; }
+        public string SP { get; set; }
+        public string StyleID { get; set; }
+        public string BrandID { get; set; }
+        public int TotalSPQty { get; set; }
+        public string Carton { get; set; }
+        public int AvailableQty { get; set; }
+        public string AQLPlan  { get; set; }
+
+        public List<FinalInspectionDefectItem> ListDefectItem { get; set; }
+
+        public List<BACriteriaItem> ListBACriteriaItem { get; set; }
+
+        public List<ViewMoistureResult> ListViewMoistureResult { get; set; }
+
+        public string MeasurementUnit { get; set; }
+
+        public List<MeasurementViewItem> ListMeasurementViewItem { get; set; }
+    }
+}
