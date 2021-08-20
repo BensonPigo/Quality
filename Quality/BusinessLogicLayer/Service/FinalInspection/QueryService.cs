f04a488f7fa4185e2da5442535a41f5708de6007
DOMAIN\benson.chung <benson.chung@sportscity.com.tw>
2021-08-20 10:12:41 +0800

    ISP20211083 - 新增Query Service

---
+++ Quality/BusinessLogicLayer/Service/FinalInspection/QueryService.cs
@@ -0,0 +1,25 @@
+﻿using DatabaseObject.ViewModel.FinalInspection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Service.FinalInspection
+{
+    public class QueryService
+    {
+        //寄信
+        public bool SendMail (DatabaseObject.ManufacturingExecutionDB.FinalInspection Req)
+        {
+
+            //透過Req從後端取得資料
+            QueryReport data = new QueryReport();
+            
+            //開始寫信
+
+            return true;
+        }
+        
+    }
+}
