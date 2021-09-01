11056b5d63b7bb870b85e417e5eb36e940530685
DOMAIN\shan.lin <shan.lin@sportscity.com.tw>
2021-09-01 10:00:52 +0800

    update

--- Quality/Quality/Areas/BulkFGT/Controllers/PullingTestController.cs
+++ Quality/Quality/Areas/BulkFGT/Controllers/PullingTestController.cs
@@ -10,6 +10,8 @@ using NPOI.SS.UserModel;
 using BusinessLogicLayer.Interface.BulkFGT;
 using BusinessLogicLayer.Service.BulkFGT;
 using DatabaseObject.ViewModel.BulkFGT;
+using FactoryDashBoardWeb.Helper;
+using System.Linq;
 
 namespace Quality.Areas.BulkFGT.Controllers
 {
@@ -18,21 +20,54 @@ namespace Quality.Areas.BulkFGT.Controllers
         // GET: BulkFGT/PullingTest
         public ActionResult Index()
         {
-            return View();
+            PullingTest_ViewModel Result = new PullingTest_ViewModel()
+            {
+                BrandID = string.Empty,
+                SeasonID = string.Empty,
+                StyleID = string.Empty,
+                Article = string.Empty,
+                ReportNo_Source = new List<string>(),
+                Detail = new PullingTest_Result(),
+            };
+
+            ViewBag.ReportNo_Source = new SetListItem().ItemListBinding(Result.ReportNo_Source);
+            return View(Result);
         }
 
         [HttpPost]
         [MultipleButton(Name = "action", Argument = "Query")]
         public ActionResult Query(PullingTest_ViewModel Req)
         {
-            return View();
+            PullingTest_ViewModel Result = new PullingTest_ViewModel()
+            {
+                BrandID = Req.BrandID,
+                SeasonID = Req.SeasonID,
+                StyleID = Req.StyleID,
+                Article = Req.Article,
+                ReportNo_Source = "5,6,7".Split(',').ToList(),
+                Detail = new PullingTest_Result(),
+            };
+
+            ViewBag.ReportNo_Source = new SetListItem().ItemListBinding(Result.ReportNo_Source);
+            return View("Index", Result);
         }
 
         [HttpPost]
         [MultipleButton(Name = "action", Argument = "Edit")]
         public ActionResult EditSave(PullingTest_ViewModel Req)
         {
-            return View();
+            PullingTest_ViewModel Result = new PullingTest_ViewModel()
+            {
+                BrandID = string.Empty,
+                SeasonID = string.Empty,
+                StyleID = string.Empty,
+                Article = string.Empty,
+                ReportNo_Source = "1,2,3,4".Split(',').ToList(),
+                Detail = new PullingTest_Result(),
+            };
+
+            ViewBag.ReportNo_Source = new SetListItem().ItemListBinding(Result.ReportNo_Source);
+            return View("Index", Result);
         }
 
         [HttpPost]
