2b626f164ffe777ac491966c88953099475f2fd0
Victor <victor.lin@sportscity.com.tw>
2021-09-01 11:42:15 +0800

    Bulk FGT Mail Group layout

--- Quality/Quality/Areas/BulkFGT/Controllers/BulkFGTMailGroupController.cs
+++ Quality/Quality/Areas/BulkFGT/Controllers/BulkFGTMailGroupController.cs
@@ -36,6 +36,15 @@ namespace Quality.Areas.BulkFGT.Controllers
                 CcAddress = "",
             };
 
+            Quality_MailGroup quality_MailTest = new Quality_MailGroup()
+            {
+                FactoryID = "VSR",
+                Type = "BulkFGT",
+                GroupName = "ABC",
+                ToAddress = "ABC@sportscity.com.tw",
+                CcAddress = "DEF@sportscity.com.tw",
+            };
+
             List<SelectListItem> FactoryList = new SetListItem().ItemListBinding(this.Factorys);
             ViewBag.FactoryList = FactoryList;
 
@@ -48,6 +57,7 @@ namespace Quality.Areas.BulkFGT.Controllers
 
             _BulkFGTMailGroup_Service.MailGroupSave(quality_Mail, BulkFGTMailGroup_Service.SaveType.Delete);
             quality_MailGroups.Add(quality_Mail);
+            quality_MailGroups.Add(quality_MailTest);
             return View(quality_MailGroups);
         }
 
@@ -55,17 +65,28 @@ namespace Quality.Areas.BulkFGT.Controllers
         [HttpPost]
         public ActionResult GetDetail(string Factory, string GroupName)
         {
-            List<SelectListItem> FactoryList = new SetListItem().ItemListBinding(this.Factorys);
-            ViewBag.FactoryList = FactoryList;
+
             Quality_MailGroup quality_Mail = new Quality_MailGroup();
 
             if (!string.IsNullOrEmpty(Factory))
             {
-                quality_Mail.FactoryID = "ESP";
-                quality_Mail.Type = "BulkFGT";
-                quality_Mail.GroupName = "ttttTest";
-                quality_Mail.ToAddress = "jack.hsu@sportscity.com.tw";
-                quality_Mail.CcAddress = "";
+                if (Factory == "ESP")
+                {
+                    quality_Mail.FactoryID = "ESP";
+                    quality_Mail.Type = "BulkFGT";
+                    quality_Mail.GroupName = "ttttTest";
+                    quality_Mail.ToAddress = "jack.hsu@sportscity.com.tw";
+                    quality_Mail.CcAddress = "";
+                }
+                else
+                {
+                    quality_Mail.FactoryID = "VSR";
+                    quality_Mail.Type = "BulkFGT";
+                    quality_Mail.GroupName = "ABC";
+                    quality_Mail.ToAddress = "ABC@sportscity.com.tw";
+                    quality_Mail.CcAddress = "DEF@sportscity.com.tw";
+                }
+
             }
 
             return Json(quality_Mail);
