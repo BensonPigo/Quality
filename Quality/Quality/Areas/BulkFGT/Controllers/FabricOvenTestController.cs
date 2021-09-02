b30a62f858ddfbbc4ec78cebbb5639d5591fed4d
Victor <victor.lin@sportscity.com.tw>
2021-09-02 15:57:27 +0800

    FabricOvenTest

--- Quality/Quality/Areas/BulkFGT/Controllers/FabricOvenTestController.cs
+++ Quality/Quality/Areas/BulkFGT/Controllers/FabricOvenTestController.cs
@@ -1,4 +1,8 @@
-﻿using Quality.Controllers;
+﻿using BusinessLogicLayer.Interface.BulkFGT;
+using BusinessLogicLayer.Service;
+using DatabaseObject.ResultModel;
+using FactoryDashBoardWeb.Helper;
+using Quality.Controllers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +13,13 @@ namespace Quality.Areas.BulkFGT.Controllers
 {
     public class FabricOvenTestController : BaseController
     {
+        private IFabricOvenTestService _FabricOvenTestService;
+        private List<string> Results = new List<string>() { "Pass", "Fail" };
+        private List<string> Temperatures = new List<string>() { "70", "90" };
+        private List<string> Times = new List<string>() { "4", "24","48" };
         public FabricOvenTestController()
         {
+            _FabricOvenTestService = new FabricOvenTestService();
             this.SelectedMenu = "Bulk FGT";
             ViewBag.OnlineHelp = this.OnlineHelp + "BulkFGT.FabricOvenTest,,";
         }
@@ -18,12 +27,43 @@ namespace Quality.Areas.BulkFGT.Controllers
         // GET: BulkFGT/FabricOvenTest
         public ActionResult Index()
         {
-            return View();
+
+            FabricOvenTest_Result model = new FabricOvenTest_Result()
+            {
+                Main = new FabricOvenTest_Main(),
+                Details = new List<FabricOvenTest_Detail>()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string POID)
+        {
+
+            FabricOvenTest_Result model = _FabricOvenTestService.GetFabricOvenTest_Result("21051739BB");
+
+            return View(model);
         }
 
         public ActionResult Detail()
         {
-            return View();
-        }
+
+            FabricOvenTest_Detail_Result model = _FabricOvenTestService.GetFabricOvenTest_Detail_Result("21051739BB","");
+
+            List<SelectListItem> ScaleIDList = new SetListItem().ItemListBinding(model.ScaleIDs);
+            List<SelectListItem> ResultChangeList = new SetListItem().ItemListBinding(Results);
+            List<SelectListItem> ResultStainList = new SetListItem().ItemListBinding(Results);
+            List<SelectListItem> TemperatureList = new SetListItem().ItemListBinding(Temperatures);
+            List<SelectListItem> TimeList = new SetListItem().ItemListBinding(Times);
+
+            ViewBag.ChangeScaleList = ScaleIDList;
+            ViewBag.ResultChangeList = ResultChangeList;
+            ViewBag.StainingScaleList = ScaleIDList;
+            ViewBag.ResultStainList = ResultStainList;
+            ViewBag.TemperatureList = TemperatureList;
+            ViewBag.TimeList = TimeList;
+            return View(model);
     }
+}
 }
