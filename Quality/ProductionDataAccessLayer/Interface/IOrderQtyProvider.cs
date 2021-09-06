67b45f20e878ddc78358d6bcf3a1a3f864cdf772
DOMAIN\jeff.yeh <Jeff.Yeh@sportscity.com.tw>
2021-09-06 10:38:07 +0800

    ISP20211067 Moclup 增加取得Orders Moclup 增加取得Order_Qty distinct Article

--- Quality/ProductionDataAccessLayer/Interface/IOrderQtyProvider.cs
+++ Quality/ProductionDataAccessLayer/Interface/IOrderQtyProvider.cs
@@ -1,6 +1,10 @@
+using DatabaseObject.ProductionDB;
+using System.Collections.Generic;
+
 namespace ProductionDataAccessLayer.Interface
 {
     public interface IOrderQtyProvider
     {
+        IList<Order_Qty> GetDistinctArticle(Order_Qty Item);
     }
 }
