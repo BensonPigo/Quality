4512b71c3f72a724f2b1b2eb041b861171c9819c
Victor <victor.lin@sportscity.com.tw>
2021-09-07 10:55:35 +0800

    fabricColorFastness

--- Quality/Quality/Areas/BulkFGT/Controllers/FabricColorFastnessController.cs
+++ Quality/Quality/Areas/BulkFGT/Controllers/FabricColorFastnessController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using BusinessLogicLayer.Interface.BulkFGT;
+using BusinessLogicLayer.Service.BulkFGT;
+using DatabaseObject.ViewModel.BulkFGT;
+using Quality.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +10,37 @@ using System.Web.Mvc;
 
 namespace Quality.Areas.BulkFGT.Controllers
 {
-    public class FabricColorFastnessController : Controller
+    public class FabricColorFastnessController : BaseController
     {
+        private IFabricColorFastness_Service _FabricColorFastness_Service;
         // GET: BulkFGT/FabricColorFastness
+
+        public FabricColorFastnessController()
+        {
+            _FabricColorFastness_Service = new FabricColorFastness_Service();
+
+        }
+
+
         public ActionResult Index()
         {
-            return View();
+
+            FabricColorFastness_ViewModel model = new FabricColorFastness_ViewModel()
+            {
+                ColorFastness_MainList = new List<ColorFastness_Result>()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string POID)
+        {
+
+            FabricColorFastness_ViewModel model = _FabricColorFastness_Service.Get_Main("21051739BB");
+
+            ViewBag.POID = POID;
+            return View(model);
         }
 
         public ActionResult Detail()
