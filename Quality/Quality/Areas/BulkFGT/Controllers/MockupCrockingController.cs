ce01e62a9db2f215ef22febfa6d08421da55a253
DOMAIN\shan.lin <shan.lin@sportscity.com.tw>
2021-09-10 10:48:57 +0800

    [ISF20210978] update

--- Quality/Quality/Areas/BulkFGT/Controllers/MockupCrockingController.cs
+++ Quality/Quality/Areas/BulkFGT/Controllers/MockupCrockingController.cs
@@ -55,11 +55,6 @@ namespace Quality.Areas.BulkFGT.Controllers
         public ActionResult Query(MockupCrocking_ViewModel Req)
         {
             //example:ADIDAS, 19FW, F1915KYB012, ED5770
-            //因為SMSURUF18BJM07M 不存在，但查得結果，這邊重設修件
-            //Req.Request.BrandID = "ADIDAS";
-            //Req.Request.SeasonID = "18FW";
-            //Req.Request.StyleID = "SMSURUF18BJM07M";
-            //Req.Request.Article = "DY3851";
 
             BusinessLogicLayer.Interface.BulkFGT.IMockupCrockingService _MockupCrockingService = new MockupCrockingService();
             MockupCrocking_ViewModel mockupCrocking_ViewModel = _MockupCrockingService.GetMockupCrocking(Req.Request);
@@ -125,6 +120,7 @@ namespace Quality.Areas.BulkFGT.Controllers
             return Json(GetArtworkTypeIDList(brandID, seasonID, styleID));
         }
 
+        [HttpPost]
         private List<SelectListItem> GetArtworkTypeIDList(string brandID, string seasonID, string styleID)
         {
             IMockupCrockingService mockupCrockingService = new MockupCrockingService();
@@ -136,5 +132,118 @@ namespace Quality.Areas.BulkFGT.Controllers
             };
             return mockupCrockingService.GetArtworkTypeID(styleArtwork_Request);
         }
+
+        [HttpPost]
+        public JsonResult SPBlur(string POID)
+        {
+            string BrandID = string.Empty;
+            string SeasonID = string.Empty;
+            string StyleID = string.Empty;
+            string Article = string.Empty;
+
+            IMockupCrockingService mockupCrockingService = new MockupCrockingService();
+
+            Orders order = new Orders();
+            order.ID = POID;
+            List<Orders> orderResult = mockupCrockingService.GetOrders(order);
+
+            if (orderResult.Count == 0)
+            {
+                return Json(new { ErrMsg = $"Cannot found SP# {POID}." });
+            }
+
+            if (orderResult.Count == 1)
+            {
+                BrandID = orderResult.FirstOrDefault().BrandID;
+                SeasonID = orderResult.FirstOrDefault().SeasonID;
+                StyleID = orderResult.FirstOrDefault().StyleID;
+
+                Order_Qty order_qty = new Order_Qty();
+                order_qty.ID = POID;
+                List<Order_Qty> order_qtyResult = mockupCrockingService.GetDistinctArticle(order_qty);
+
+                if (order_qtyResult.Count == 1)
+                {
+                    Article = order_qtyResult.FirstOrDefault().Article;
+                }
+            }
+
+            return Json(new { ErrMsg = "", BrandID = BrandID, SeasonID = SeasonID, StyleID = StyleID, Article = Article });
+        }
+
+        public ActionResult AddDetailRow(int lastNo)
+        {
+            MockupCrocking_ViewModel mockupCrocking_ViewModel = new MockupCrocking_ViewModel();
+            List<string> resultOption = new List<string>();
+            foreach (var item in mockupCrocking_ViewModel.Result_Source)
+            {
+                resultOption.Add($"<option value='{item.Value}'>{item.Text}</option>");
+            }
+
+            List<string> scaleOption = new List<string>();
+            foreach (var item in mockupCrocking_ViewModel.Scale_Source)
+            {
+                scaleOption.Add($"<option value='{item.Value}'>{item.Text}</option>");
+            }
+
+            lastNo = lastNo + 1;
+            string html = string.Empty;
+            html += $"<tr idx='{lastNo}' class='row-content' style='vertical-align: middle; text-align: center;'>";
+            html += "<td>";
+            html += "<div class='input-group'>";
+            html += $"<input id='MockupCrocking_Detail_{lastNo}__Design' name='MockupCrocking_Detail[{lastNo}].Design' type='text' value=''>";
+            html += "</div>";
+            html += "</td>";
+            html += "<td>";
+            html += "<div class='input-group'>";
+            html += $"<input id='MockupCrocking_Detail_{lastNo}__ArtworkColor' class='AFColor' name='MockupCrocking_Detail[{lastNo}].ArtworkColor' type='hidden' value=''>";
+            html += $"<input id='MockupCrocking_Detail_{lastNo}__ArtworkColorName' class='AFColor' name='MockupCrocking_Detail[{lastNo}].ArtworkColorName' readonly='readonly' type='text' value=''>";
+            html += $"<input type='button' class='site-btn btn-blue btnArtworkColorItem' style='margin:0;border:0;' value='...'>";
+            html += "</div>";
+            html += "</td>";
+            html += "<td>";
+            html += $"<input id='MockupCrocking_Detail_{lastNo}__FabricRefNo' name='MockupCrocking_Detail[{lastNo}].FabricRefNo' type='text' value=''>";
+            html += "</td>";
+            html += "<td>";
+            html += "<div class='input-group'>";
+            html += $"<input id='MockupCrocking_Detail_{lastNo}__FabricColor' class='AFColor' name='MockupCrocking_Detail[{lastNo}].FabricColor' type='hidden' value=''>";
+            html += $"<input id='MockupCrocking_Detail_{lastNo}__FabricColorName' class='AFColor' name='MockupCrocking_Detail[{lastNo}].FabricColorName' readonly='readonly' type='text' value=''>";
+            html += $"<input type='button' class='site-btn btn-blue btnFabricColorItem' style='margin:0;border:0;' value='...'>";
+            html += "</div>";
+            html += "</td>";
+            html += "<td>";
+
+            html += $"<select id='MockupCrocking_Detail_{lastNo}__DryScale' name='Model.MockupCrocking_Detail[{lastNo}].DryScale' class='NotEdit' style='width:157px;'>";
+            html += string.Join("", scaleOption);
+            html += "</select >";
+
+            html += "</td>";
+            html += "<td>";
+
+            html += $"<select id='MockupCrocking_Detail_{lastNo}__WetScale' name='Model.MockupCrocking_Detail[{lastNo}].WetScale' class='NotEdit' style='width:157px;'>";
+            html += string.Join("", scaleOption);
+            html += "</select>";
+
+            html += "<td>";
+
+            html += $"<select id='MockupCrocking_Detail_{lastNo}__Result' class='result' name='Model.MockupCrocking_Detail[{lastNo}].Result' style='width:157px;' onchange='changeResult()'>";
+            html += string.Join("", resultOption);
+            html += "</select>";
+
+            html += "</td >";
+            html += "<td>";
+            html += $"<input id='MockupCrocking_Detail_{lastNo}__Remark' name='MockupCrocking_Detail[{lastNo}].Remark' type='text' value=''>";
+            html += "</td>";
+            html += "<td>";
+            html += $"<input id='MockupCrocking_Detail_{lastNo}__LastUpdate' name='MockupCrocking_Detail[{lastNo}].LastUpdate' readonly='readonly' type='text' value=''>";
+            html += "</td>";
+            html += "<td>";
+            html += "<img class='detailDelete' src='/Image/Icon/Delete.png' width='30' style='min-width:30px'>";
+            html += "</td>";
+            html += "</tr>";
+            return Content(html);
+        }
+
+ 
     }
 }
