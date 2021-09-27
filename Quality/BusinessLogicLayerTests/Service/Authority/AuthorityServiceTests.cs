2b6a63668f7ed62e576e1ced76e3f03c0998c45c
Willy <willy.wei@sprotscity.com.tw>
2021-09-27 10:38:46 +0800

    調整GetAllUser 資料庫撈寫法

---
+++ Quality/BusinessLogicLayerTests/Service/Authority/AuthorityServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLogicLayer.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogicLayer.Interface;
+
+namespace BusinessLogicLayer.Service.Tests
+{
+    [TestClass()]
+    public class AuthorityServiceTests
+    {
+        [TestMethod()]
+        public void GetAlUserTest()
+        {
+            try
+            {
+                IAuthorityService _AuthorityService = new AuthorityService();
+                var result = _AuthorityService.GetAlUser("esp");
+
+                Assert.IsTrue(true);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.ToString());
+            }
+        }
+    }
+}
