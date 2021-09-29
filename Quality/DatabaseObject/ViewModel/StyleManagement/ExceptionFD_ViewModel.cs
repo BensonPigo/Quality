a865f8ef910987f04b9f5e22fe389eec186e1538
DOMAIN\benson.chung <benson.chung@sportscity.com.tw>
2021-09-29 20:01:55 +0800

    ISP20211170 - 新增Exception (FD) 功能

---
+++ Quality/DatabaseObject/ViewModel/StyleManagement/ExceptionFD_ViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseObject.ViewModel.StyleManagement
+{
+    public class ExceptionFD_ViewModel
+    {
+        public string StyleID { get; set; }
+        public string BrandID { get; set; }
+        public string SeasonID { get; set; }
+        public string Article { get; set; }
+        public string ExpectionFormStatus { get; set; }
+        public DateTime? ExpectionFormDate { get; set; }
+        public string ExpectionFormRemark { get; set; }
+    }
+}
